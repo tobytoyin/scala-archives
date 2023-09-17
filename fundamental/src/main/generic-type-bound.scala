f7ffdf26ac1db0483dcd8d46834d4f7790c87d6c
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-09-18 00:31:47 +0100

    remove comments

--- fundamental/src/main/generic-type-bound.scala
+++ fundamental/src/main/generic-type-bound.scala
@@ -1,66 +1,22 @@
 trait ItemInterface:
-    def printID(): Unit
+  def printID(): Unit
 
 class Item(id: String) extends ItemInterface:
-    def printID(): Unit = println(id) 
+  def printID(): Unit = println(id)
 
 class Container[T <: ItemInterface]:
-    var objects: List[T] = Nil
+  var objects: List[T] = Nil
 
-    def add(item: T): Unit = 
-        objects = objects :+ item
+  def add(item: T): Unit =
+    objects = objects :+ item
 
-    def printAllID(): Unit = 
-        objects.foreach(item => item.printID())
+  def printAllID(): Unit =
+    objects.foreach(item => item.printID())
 
+@main def main(): Unit =
 
-@main def main(): Unit = 
-
-    val container = Container[Item]()
-    container.add(Item("A"))
-    container.add(Item("B"))
-    container.add(Item("C"))
-    container.printAllID()    
-
-
-
-
-
-       
-
-// // Printer to access items from Container[T]
-// trait ItemInterface:
-//     def printID(): Unit 
-
-// class Item(id: String) extends ItemInterface:
-//     def printID(): Unit = println(id)     
-
-// class ItemsContainer[A <: ItemInterface]:
-//     private var objects: List[A] = Nil
-
-//     def add(item: A): Unit = 
-//         objects = objects :+ item
-
-//     def printAllId(): Unit = 
-//         objects.foreach(item => item.printID())
-
-
-
-
-// @main def main(): Unit =
-
-
-//     val container = ItemsContainer[Item]()
-//     container.add(itemA)
-//     container.add(itemB)
-//     container.add(itemC)
-//     container.printAllId()
-
-    // val printer = ItemPrinter(container)
-
-    // println(printer.printAllId())
-
-
-    
-
-    
+  val container = Container[Item]()
+  container.add(Item("A"))
+  container.add(Item("B"))
+  container.add(Item("C"))
+  container.printAllID()
