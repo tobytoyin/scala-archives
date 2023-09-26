05aca729b7569a6b9c07c53c2769466dd4561bdc
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-09-27 00:07:30 +0100

    create an adaptee interface

--- design-patterns/src/main/ObjectAdapter.scala
+++ design-patterns/src/main/ObjectAdapter.scala
@@ -8,21 +8,28 @@ class Client:
     println("running Client method")
     target.request()
 
+    
 class ExternalModule:
   // ExternalModule represents a class that implemented/ import from external modules
   // it has their only method that are incompatible to the Client's interface
   def specificRequest(): Unit =
     println("External ModuleA request method")
 
-class Adapter[T <: ExternalModule](adaptee: T) extends TargetInterface:
+trait AdapteeInterface:
+  // an Interface for classes which contains `specificRequests`
+  def specificRequest(): Unit
+
+class Adapter[T <: AdapteeInterface](adaptee: T) extends TargetInterface:
   def request(): Unit =
     adaptee.specificRequest()
 
 @main def main(): Unit =
   val client = Client()
 
-  val externalMod = ExternalModule()
+  // as an ad-hoc way to indicate ExternalModule shares AdapteeInterface
+  class Adaptee extends ExternalModule with AdapteeInterface
+  val externalMod = Adaptee()
 
-  val adapter = Adapter[ExternalModule](externalMod)
+  val adapter = Adapter[AdapteeInterface](externalMod)
 
   client.run(adapter)
